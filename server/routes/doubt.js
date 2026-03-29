904611106451fd78ecb76c32a5674ee846af9dda
Dhanush1376 <151372966+Dhanush1376@users.noreply.github.com>
2026-03-30 01:58:09 +0530

    chore: untrack node_modules and sync project files

--- server/routes/doubt.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { answerDoubt } from '../controllers/doubt.controller.js';
-
-const router = express.Router();
-
-router.post('/doubt', answerDoubt);
-
-export default router;
