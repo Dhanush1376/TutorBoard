e10b99c357b3f3e77b63e986c85f16572c544fed
Dhanush1376 <151372966+Dhanush1376@users.noreply.github.com>
2026-03-30 01:58:09 +0530

    chore: untrack node_modules and sync project files

--- client/src/lib/utils.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-// Utility for easier tailwind class merging
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
-}
--- server/routes/generate.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { generateExplanation } from '../controllers/generate.controller.js';
-
-const router = express.Router();
-
-// Route cleanly mapped to controller logic
-router.post('/generate', generateExplanation);
-
-export default router;
